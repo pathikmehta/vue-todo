eaf19703fc0b012c83c9ae83f6895fe7798a4de9
Pathik Mehta <pathik@pirimidtech.com>
2020-03-30 09:58:51 +0530

    Added header and simple add todo component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
+    <Header />
+    <AddTodo />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
 import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 
 export default {
   name: 'App',
   components: {
-    Todos    
+    Header,
+    Todos  ,
+    AddTodo  
   },
   data() {
     return {
@@ -50,5 +56,8 @@ export default {
   body {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+  }
+  .btn:hover {
+    background: #666;
   }
 </style>
