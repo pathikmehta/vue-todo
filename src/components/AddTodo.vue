0d75e7327ddc84d8a61a19f12d22924691fafd7a
Pathik Mehta <pathik@pirimidtech.com>
2020-03-30 11:01:26 +0530

    Added axios and integrated it with get, post, delete api calls for todos

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
 export default {
   name: "AddTodo",
@@ -19,10 +19,8 @@ export default {
   },
   methods: {
     addTodo(e) {
-      console.log('Inside add todo method');
       e.preventDefault();
       const newTodo = {
-        id: uuidv4(),
         title: this.title,
         completed: false
       }
